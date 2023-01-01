1a6689dc62a32265a56d55cc57da1e44a9b74c02
Kevaron <kevtapia75@gmail.com>
2023-01-01 13:23:32 -0600

    changes

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="header">
+        <div class="header__logo"></div>
+        <div class="header__links"></div>
+        <div class="header__cta"></div>
+    </div>
+</template>
